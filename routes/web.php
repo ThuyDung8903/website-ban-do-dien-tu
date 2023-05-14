62e83c43048d91bf2f592933af7256446659d29f
Thuy Dung <thuydung8ctmn@gmail.com>
2023-05-14 12:49:28 +0700

    login-logout admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -20,18 +19,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('/admin')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-});
-
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+require_once __DIR__ . '/auth.php';
+require_once __DIR__ . '/be.php';
 
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\Frontend\FrontendController::class, 'index']);
+Route::get('/', [App\Http\Controllers\Frontend\FrontendController::class, 'index'])->name('home');
 Route::get('/collections', [App\Http\Controllers\Frontend\FrontendController::class, 'categories']);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
 
