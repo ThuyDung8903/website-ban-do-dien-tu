bdbf4cb1e7c10b820b088d9b41f8bd74f8a82448
Nguyen Xuan Hoang <hoangnx.hut@gmail.com>
2023-05-17 18:38:32 +0700

    single product view in livewire

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -30,5 +30,19 @@ class FrontendController extends Controller
         }
     }
 
-
+    public function productView(string $category_slug, string $product_slug)
+    {
+        $category = Category::where('slug', $category_slug)->where('status', '1')->first();
+        if ($category) {
+            $product = $category->products()->where('slug', $product_slug)->first();
+            if ($product) {
+                return view('frontend.collections.products.view', compact('category', 'product'));
+            } else {
+                return redirect()->back();
+            }
+            return view('frontend.collections.product.view', compact('category', 'product'));
+        } else {
+            return redirect()->back();
+        }
+    }
 }
--- routes/fe.php
+++ routes/fe.php
@@ -1,10 +1,14 @@
 <?php
 //<!--Route for front end-->
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 Auth::routes();
 
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/', [App\Http\Controllers\Frontend\FrontendController::class, 'index']);
 Route::get('/collections', [App\Http\Controllers\Frontend\FrontendController::class, 'categories'])->name('collections.categories');
-Route::get('/collections/{category_slug}', [App\Http\Controllers\Frontend\FrontendController::class, 'products'])->name('collections.category-slug.products');
-Route::get('/collections/{id}', [App\Http\Controllers\Frontend\FrontendController::class, 'productsByCategoryId'])->name('collections.category-id.products');
+Route::get('/collections/{category_slug}', [App\Http\Controllers\Frontend\FrontendController::class, 'products'])->name('collections.products');
+Route::get('/collections/{category_slug}/{product_slug}', [App\Http\Controllers\Frontend\FrontendController::class, 'productView'])->name('collection.product.view');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
